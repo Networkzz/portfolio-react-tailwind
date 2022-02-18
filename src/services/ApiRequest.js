519b4226acb53cc8accc2932324e74a29819b37a
Networkz <alex.cinco99@gmail.com>
2022-02-18 13:05:36 +0100

    Add: new way to fetch api, not working yet

--- src/services/ApiRequest.js
+++ src/services/ApiRequest.js
@@ -1,15 +1,15 @@
 import React from "react";
-
 const apiURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1&page=1&sparkline=false"
 let bitcoinPrice = {price: ""};
 fetch(apiURL)
   .then(response => response.json())
   .then(coins => {
     coins.forEach((coin) => {
-      bitcoinPrice.price = coin.current_price;
+      bitcoinPrice = coin.current_price;
     });
-    return bitcoinPrice.price;
-  }) 
+    return bitcoinPrice;
+  })
+
 function ApiRequest() {
   return (
     <span>{bitcoinPrice.price + "$"}</span>
