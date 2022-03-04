311bd6055078847eda459c3fad0f447336bd4569
Networkz <alex.cinco99@gmail.com>
2022-03-04 14:30:18 +0100

    Delete: unused imports

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import Header from './components/Header';
 import About from './components/About/About';
 import Bio from  './components/Bio'
 import Socials from './components/Socials';
-import BitcoinContainer from './components/Interactive/BitcoinContainer';
 import Portofolio from './components/Portfolio/Portofolio';
 import Portofolio2 from './components/Portfolio/Portofolio2';
-import TodoList from './components/Utility/TodoList';
 function App() {
   return (
     <div className="bg-gray-700">
